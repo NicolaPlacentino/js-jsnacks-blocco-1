c48f71f96e079f0377cde52f2181a7c55340a7aa
NicolaPlacentino <nyko0921@gmail.com>
2022-12-13 15:59:35 +0100

    first commit

---
+++ jsnack2/script-2.js
@@ -0,0 +1,4 @@
+/*SNACK 2
+In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, 
+l’utente inserisce il suo nome tramite un input.
+Comunicagli se può partecipare o no alla festa.*/
